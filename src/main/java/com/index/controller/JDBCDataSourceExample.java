af78696b899595d743936c59a655701a315f5e36
yi <yi@yi-HP-3120>
2017-12-19 16:28:02 +0000

    try datasource 1

---
+++ src/main/java/com/index/controller/JDBCDataSourceExample.java
@@ -0,0 +1,136 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.index.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+
+/**
+ *
+ * @author yi
+ */
+public class JDBCDataSourceExample extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+
+        
+        Context ctx = null;
+		Connection con = null;
+		Statement stmt = null;
+		ResultSet rs = null;
+		try{
+			ctx = new InitialContext();
+			DataSource ds = (DataSource) ctx.lookup("java:/comp/env/jdbc/MyLocalDB");
+			
+			con = ds.getConnection();
+			stmt = con.createStatement();
+			
+			rs = stmt.executeQuery("select id, title from MOVIES");
+			
+			PrintWriter out = response.getWriter();
+            response.setContentType("text/html");
+            out.print("<html><body><h2>Employee Details</h2>");
+            out.print("<table border=\"1\" cellspacing=10 cellpadding=5>");
+            out.print("<th>Employee ID</th>");
+            out.print("<th>Employee Name</th>");
+            
+            while(rs.next())
+            {
+                out.print("<tr>");
+                out.print("<td>" + rs.getInt("id") + "</td>");
+                out.print("<td>" + rs.getString("title") + "</td>");
+                out.print("</tr>");
+            }
+            out.print("</table></body><br/>");
+            
+            //lets print some DB information
+            out.print("<h3>Database Details</h3>");
+            out.print("Database Product: "+con.getMetaData().getDatabaseProductName()+"<br/>");
+            out.print("Database Driver: "+con.getMetaData().getDriverName());
+            out.print("</html>");
+            
+		}catch(NamingException e){
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}finally{
+			try {
+				rs.close();
+				stmt.close();
+				con.close();
+				ctx.close();
+			} catch (SQLException e) {
+				System.out.println("Exception in closing DB resources");
+			} catch (NamingException e) {
+				System.out.println("Exception in closing Context");
+			}
+			
+		}
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
